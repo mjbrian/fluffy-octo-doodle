7d41e7ac95c4e1f7d9f467ee1ada3da939679db1
matt brian <matt.brian@mindbodyonline.com>
2019-12-31 11:45:48 -0800

    fixing styles and routes

--- myapp/controllers/landing.js
+++ myapp/controllers/landing.js
@@ -15,7 +15,7 @@ exports.submit_lead = function (req, res, next) {
 
 exports.show_leads = function(req, res, next) {
 	return models.Lead.findAll().then(leads => {
- 		res.render('landing', { title: 'Express', leads: leads });
+ 		res.render('lead/leads', { title: 'Express', leads: leads });
 	});
 }
 
@@ -75,7 +75,6 @@ exports.delete_lead_json = function(req, res, next) {
             id: req.params.lead_id
         }
     }).then(result => {
-        // res.redirect('/leads');
         res.send({msg: "Success!"});
     });
 }
